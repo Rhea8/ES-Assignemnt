1c39a7bc801e8d96da13a3b77bf524f67b366864
Rhea8 <72485818+Rhea8@users.noreply.github.com>
2021-01-13 23:39:23 +0530

    Add files via upload

--- master1.ino
+++ master1.ino
@@ -1,40 +1,57 @@
-/*
-  I2C Master Control Demo
-  i2c-master-demo-control.ino
-  Demonstrate use of I2C bus
-  Master sends potentimeter position data
-  DroneBot Workshop 2019
-  https://dronebotworkshop.com
-*/
 
-// Include Arduino Wire library for I2C
-#include <Wire.h>
+//SPI MASTER (ARDUINO)
+//SPI COMMUNICATION BETWEEN TWO ARDUINO 
+//CIRCUIT DIGEST
 
-// Define Slave I2C Address
-#define SLAVE_ADDR 9
+#include<SPI.h>                             //Library for SPI 
+#define LED 7           
+#define ipbutton 2
+int buttonvalue;
+int x;
+void setup (void)
 
-// Analog pin for potentiometer
-int analogPin = 0;
-// Integer to hold potentiometer value
-int val = 0;
-
-void setup() {
-
-  // Initialize I2C communications as Master
-  Wire.begin();
+{
+  Serial.begin(115200);                   //Starts Serial Communication at Baud Rate 115200 
   
+  pinMode(ipbutton,INPUT);                //Sets pin 2 as input 
+  pinMode(LED,OUTPUT);                    //Sets pin 7 as Output
+  
+  SPI.begin();                            //Begins the SPI commnuication
+  SPI.setClockDivider(SPI_CLOCK_DIV8);    //Sets clock for SPI communication at 8 (16/8=2Mhz)
+  digitalWrite(SS,HIGH);                  // Setting SlaveSelect as HIGH (So master doesnt connnect with slave)
 }
 
-void loop() {
-  delay(50);
-  
-  // Read pot value 
-  // Map to range of 1-255 for flash rate
-    val = map(analogRead(analogPin), 0, 1023, 255, 1);
-    
-  // Write a charatre to the Slave
-  Wire.beginTransmission(SLAVE_ADDR);
-  Wire.write(val);
-  Wire.endTransmission();
+void loop(void)
+{
+  byte Mastersend,Mastereceive;          
 
+  buttonvalue = digitalRead(ipbutton);   //Reads the status of the pin 2
+
+  if(buttonvalue == HIGH)                //Logic for Setting x value (To be sent to slave) depending upon input from pin 2
+  {
+    x = 1;
+  }
+  else
+  {
+    x = 0;
+  }
+  
+  digitalWrite(SS, LOW);                  //Starts communication with Slave connected to master
+  
+  Mastersend = x;                            
+  Mastereceive=SPI.transfer(Mastersend); //Send the mastersend value to slave also receives value from slave
+  
+  if(Mastereceive == 1)                   //Logic for setting the LED output depending upon value received from slave
+  {
+    digitalWrite(LED,HIGH);              //Sets pin 7 HIGH
+    Serial.println("Master LED ON");
+  }
+  else
+  {
+   digitalWrite(LED,LOW);               //Sets pin 7 LOW
+   Serial.println("Master LED OFF");
+  }
+  delay(1000);
 }
+
+
